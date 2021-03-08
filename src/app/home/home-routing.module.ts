42d691479178c3cfe21b6c63805867a161af670a
Muhammad Bilal Shah <bilalshah0000bs@gmail.com>
2021-03-08 14:53:36 +0500

    v1 done

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,7 +6,32 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children:[
+      {
+        path:'',
+        redirectTo:'clock',
+        pathMatch:'full'
+      },
+
+      {
+        path:'clock',
+        loadChildren: () => import('./../clock/clock.module').then(m => m.ClockModule),
+      },
+      {
+        path:'stopwatch',
+        loadChildren: () => import('./../stop-watch/stop-watch.module').then(m => m.StopWatchModule),
+      },
+      {
+        path:'counter',
+        loadChildren: () => import('./../counter/counter.module').then(m => m.CounterModule),
+      },
+      {
+        path:'interval',
+        loadChildren: () => import('./../interval/interval.module').then(m => m.IntervalModule),
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
